dde476f7805f349d0508a6c25fc7e880e7aaf388
必為 陳 <chenbewill@hotmail.com>
2020-07-05 23:25:26 +0800

    V3

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication1.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "V2222";
+            ViewBag.Message = "V2333";
 
             return View();
         }
